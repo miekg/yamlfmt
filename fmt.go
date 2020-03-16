d423ec69f803bcd6599f88df4d9eb7c3143fda20
Yi Wang <yi.wang.2005@gmail.com>
2020-03-16 16:44:40 -0700

    Update fmt.go

--- fmt.go
+++ fmt.go
@@ -2,53 +2,72 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 
 	"gopkg.in/yaml.v3"
 )
 
-var (
-	strict = flag.Bool("strict", false, "be strict when parsing")
-	indent = flag.Uint("indent", 2, "default indent")
-)
-
 func main() {
+	overwrite := flag.Bool("w", false, "overwrite the input file")
+	indent := flag.Int("indent", 2, "default indent")
 	flag.Parse()
-	decoders := []*yaml.Decoder{}
 
-	if flag.NArg() == 0 {
-		decoders = append(decoders, yaml.NewDecoder(os.Stdin))
-	}
 	for _, f := range flag.Args() {
-		r, err := os.Open(f)
-		if err != nil {
-			log.Fatal(err)
-		}
-		decoders = append(decoders, yaml.NewDecoder(r))
+		formatFile(f, *indent, *overwrite)
+	}
+}
+
+func formatFile(f string, indent int, overwrite bool) {
+	r, err := os.Open(f)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	for _, d := range decoders {
-		in := yaml.Node{}
-		err := d.Decode(&in)
-		for err == nil {
-			e := yaml.NewEncoder(os.Stdout)
-			e.SetIndent(int(*indent))
-			if err := e.Encode(&in); err != nil {
-				log.Fatal(err)
-			}
-			e.Close()
+	var out bytes.Buffer
+	if e := formatStream(r, &out, indent); e != nil {
+		log.Fatalf("Failed formatting YAML stream: %v", e)
+	}
 
-			if err = d.Decode(&in); err == nil {
-				fmt.Println("---\n")
-			}
-		}
+	r.Close()
 
-		if err != nil && err != io.EOF {
+	if e := dumpStream(&out, f, overwrite); e != nil {
+		log.Fatalf("Cannot overwrite: %v", e)
+	}
+}
+
+func formatStream(r io.Reader, out io.Writer, indent int) error {
+	d := yaml.NewDecoder(r)
+	in := yaml.Node{}
+	err := d.Decode(&in)
+	for err == nil {
+		e := yaml.NewEncoder(out)
+		e.SetIndent(indent)
+		if err := e.Encode(&in); err != nil {
 			log.Fatal(err)
 		}
+		e.Close()
+
+		if err = d.Decode(&in); err == nil {
+			fmt.Fprintln(out, "---\n")
+		}
 	}
+
+	if err != nil && err != io.EOF {
+		return err
+	}
+	return nil
+}
+
+func dumpStream(out *bytes.Buffer, f string, overwrite bool) error {
+	if overwrite {
+		return ioutil.WriteFile(f, out.Bytes(), 0744)
+	}
+	_, err := io.Copy(os.Stdout, out)
+	return err
 }
